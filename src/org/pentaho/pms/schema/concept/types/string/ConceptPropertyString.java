e5e617f8303b66cf4b5640c4e063666a866f6458
mlowery <mlowery@pentaho.com>
2007-04-30 19:23:31 +0000

    Made the default value an empty string instead of null.

--- src/org/pentaho/pms/schema/concept/types/string/ConceptPropertyString.java
+++ src/org/pentaho/pms/schema/concept/types/string/ConceptPropertyString.java
@@ -1,13 +1,13 @@
 /*
- * Copyright 2006 Pentaho Corporation.  All rights reserved. 
- * This software was developed by Pentaho Corporation and is provided under the terms 
- * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
- * this file except in compliance with the license. If you need a copy of the license, 
- * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
+ * Copyright 2006 Pentaho Corporation.  All rights reserved.
+ * This software was developed by Pentaho Corporation and is provided under the terms
+ * of the Mozilla Public License, Version 1.1, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho
  * BI Platform.  The Initial Developer is Pentaho Corporation.
  *
- * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
  * the license for the specific language governing your rights and limitations.
 */
 package org.pentaho.pms.schema.concept.types.string;
@@ -19,18 +19,18 @@ import org.pentaho.pms.schema.concept.types.ConceptPropertyType;
 public class ConceptPropertyString extends ConceptPropertyBase implements ConceptPropertyInterface, Cloneable
 {
     private String value;
-    
-    public ConceptPropertyString(String name, String value)
-    {
-        super(name);
-        this.value = value;
+    private static final String EMPTY_STRING = "";
+
+    public ConceptPropertyString(String name, String value) {
+      super(name);
+      setValue(value);
     }
 
     public String toString()
     {
         return value;
     }
-    
+
     public Object clone() throws CloneNotSupportedException
     {
         return super.clone();
@@ -46,11 +46,14 @@ public class ConceptPropertyString extends ConceptPropertyBase implements Concep
         return value;
     }
 
-    public void setValue(Object value)
-    {
+    public void setValue(Object value) {
+      if (null != value) {
         this.value = (String) value;
+      } else {
+        this.value = EMPTY_STRING;
+      }
     }
-    
+
     public boolean equals(Object obj)
     {
         return value.equals(obj);
