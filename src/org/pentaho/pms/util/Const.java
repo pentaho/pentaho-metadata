e6f9e3a0ef7fe0349436a23e895beeea90291108
Matt Casters <mcasters@pentaho.com>
2007-05-03 19:27:40 +0000

    Bumping version to 0.8.0

--- src/org/pentaho/pms/util/Const.java
+++ src/org/pentaho/pms/util/Const.java
@@ -58,7 +58,7 @@ public class Const
 	/**
 	 *  Version number
 	 */
-	public static final String VERSION = "0.7.1"; //$NON-NLS-1$
+	public static final String VERSION = "0.8.0"; //$NON-NLS-1$
 
 	/**
 	 * What's the file systems file separator on this operating system?
