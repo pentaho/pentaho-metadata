733d34d6b10f942ce78444f8c7d018b3a8e63d59
mlowery <mlowery@pentaho.com>
2007-05-03 21:30:42 +0000

    New equals and hashCode implementations.

--- src/org/pentaho/pms/schema/concept/types/number/ConceptPropertyNumber.java
+++ src/org/pentaho/pms/schema/concept/types/number/ConceptPropertyNumber.java
@@ -1,19 +1,21 @@
 /*
- * Copyright 2006 Pentaho Corporation.  All rights reserved. 
- * This software was developed by Pentaho Corporation and is provided under the terms 
- * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
- * this file except in compliance with the license. If you need a copy of the license, 
- * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
+ * Copyright 2006 Pentaho Corporation.  All rights reserved.
+ * This software was developed by Pentaho Corporation and is provided under the terms
+ * of the Mozilla Public License, Version 1.1, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho
  * BI Platform.  The Initial Developer is Pentaho Corporation.
  *
- * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
  * the license for the specific language governing your rights and limitations.
 */
 package org.pentaho.pms.schema.concept.types.number;
 
 import java.math.BigDecimal;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.pentaho.pms.schema.concept.ConceptPropertyInterface;
 import org.pentaho.pms.schema.concept.types.ConceptPropertyBase;
 import org.pentaho.pms.schema.concept.types.ConceptPropertyType;
@@ -21,7 +23,7 @@ import org.pentaho.pms.schema.concept.types.ConceptPropertyType;
 public class ConceptPropertyNumber extends ConceptPropertyBase implements ConceptPropertyInterface, Cloneable
 {
     private BigDecimal value;
-    
+
     public ConceptPropertyNumber(String name, BigDecimal value)
     {
         super(name);
@@ -54,7 +56,7 @@ public class ConceptPropertyNumber extends ConceptPropertyBase implements Concep
         if (value==null) return null;
         return value.toString();
     }
-    
+
     public Object clone() throws CloneNotSupportedException
     {
       ConceptPropertyNumber rtn = (ConceptPropertyNumber) super.clone();
@@ -78,14 +80,19 @@ public class ConceptPropertyNumber extends ConceptPropertyBase implements Concep
     {
         this.value = (BigDecimal) value;
     }
-    
-    public boolean equals(Object obj)
-    {
-        return value.equals(obj);
+
+    public boolean equals(Object obj) {
+      if (obj instanceof ConceptPropertyNumber == false) {
+        return false;
+      }
+      if (this == obj) {
+        return true;
+      }
+      ConceptPropertyNumber rhs = (ConceptPropertyNumber) obj;
+      return new EqualsBuilder().append(value, rhs.value).isEquals();
     }
 
-    public int hashCode()
-    {
-        return value.hashCode();
+    public int hashCode() {
+      return new HashCodeBuilder(137, 199).append(value).toHashCode();
     }
 }
