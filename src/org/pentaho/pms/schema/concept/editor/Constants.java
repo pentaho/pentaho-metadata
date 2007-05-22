ae94a168b63dfdb2ef85418e6289c7bf35938468
gmoran <gmoran@pentaho.com>
2007-05-22 19:24:28 +0000

    prevent localization warnings

--- src/org/pentaho/pms/schema/concept/editor/Constants.java
+++ src/org/pentaho/pms/schema/concept/editor/Constants.java
@@ -42,30 +42,30 @@ public class Constants {
 
   private static void initFontRegistry(final Display display) {
     fontRegistry = new FontRegistry(display);
-    fontRegistry.put("card-title", new FontData[] { new FontData("Tahoma", 10, SWT.BOLD) });
-    fontRegistry.put("group-name", new FontData[] { new FontData("Tahoma", 12, SWT.BOLD) });
-    fontRegistry.put("prop-mgmt-title", new FontData[] { new FontData("Tahoma", 10, SWT.BOLD) });
+    fontRegistry.put("card-title", new FontData[] { new FontData("Tahoma", 10, SWT.BOLD) }); //$NON-NLS-1$ //$NON-NLS-2$
+    fontRegistry.put("group-name", new FontData[] { new FontData("Tahoma", 12, SWT.BOLD) }); //$NON-NLS-1$ //$NON-NLS-2$
+    fontRegistry.put("prop-mgmt-title", new FontData[] { new FontData("Tahoma", 10, SWT.BOLD) }); //$NON-NLS-1$ //$NON-NLS-2$
   }
 
   private static void initImageRegistry(final Display display) {
     imageRegistry = new ImageRegistry(display);
-    imageRegistry.put("add-button", createImage(display, "child-property-add.png"));
-    imageRegistry.put("del-button", createImage(display, "child-property-delete.png"));
-    imageRegistry.put("override-button", createImage(display, "override.png"));
-    imageRegistry.put("stop-override-button", createImage(display, "stop-override.png"));
-    imageRegistry.put("concept-editor-app", createImage(display, "concept-editor.png"));
-    imageRegistry.put("property-group", createImage(display, "folder.png"));
+    imageRegistry.put("add-button", createImage(display, "child-property-add.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("del-button", createImage(display, "child-property-delete.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("override-button", createImage(display, "override.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("stop-override-button", createImage(display, "stop-override.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("concept-editor-app", createImage(display, "concept-editor.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("property-group", createImage(display, "folder.png")); //$NON-NLS-1$ //$NON-NLS-2$
 
-    imageRegistry.put("parent-property", createImage(display, "parent-property.png"));
-    imageRegistry.put("inherited-property", createImage(display, "inherited-property.png"));
-    imageRegistry.put("security-property", createImage(display, "security-property.png"));
-    imageRegistry.put("child-property", createImage(display, "child-property.png"));
+    imageRegistry.put("parent-property", createImage(display, "parent-property.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("inherited-property", createImage(display, "inherited-property.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("security-property", createImage(display, "security-property.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("child-property", createImage(display, "child-property.png")); //$NON-NLS-1$ //$NON-NLS-2$
 
-    imageRegistry.put("column-add-button", createImage(display, "column-add.png"));
-    imageRegistry.put("column-del-button", createImage(display, "column-delete.png"));
+    imageRegistry.put("column-add-button", createImage(display, "column-add.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("column-del-button", createImage(display, "column-delete.png")); //$NON-NLS-1$ //$NON-NLS-2$
 
-    imageRegistry.put("column", createImage(display, "column.png"));
-    imageRegistry.put("table", createImage(display, "table.png"));
+    imageRegistry.put("column", createImage(display, "column.png")); //$NON-NLS-1$ //$NON-NLS-2$
+    imageRegistry.put("table", createImage(display, "table.png")); //$NON-NLS-1$ //$NON-NLS-2$
 
   }
 
