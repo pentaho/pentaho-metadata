d9f9f8ff5cec383f29b7cdc25d38c976b4efd54b
gmoran <gmoran@pentaho.com>
2007-09-27 01:39:03 +0000

    PMD-30 backing off disabling complex joins...

--- src/org/pentaho/pms/schema/dialog/RelationshipDialog.java
+++ src/org/pentaho/pms/schema/dialog/RelationshipDialog.java
@@ -401,15 +401,8 @@ public class RelationshipDialog extends Dialog {
   public void setComplex() {
     wFromField.setEnabled(relationshipMeta.isRegular());
     wToField.setEnabled(relationshipMeta.isRegular());
-    
-    // TODO: complex joins do not generate the correct SQL, and 
-    // needs to be thoughtfully implemented; disabling the feature, 
-    // but needs to be revisited
-    
-//    wComplexJoin.setEnabled(relationshipMeta.isComplex());
-//    wlComplexJoin.setEnabled(relationshipMeta.isComplex());
-    wComplexJoin.setEnabled(false);
-    wlComplexJoin.setEnabled(false);
+    wComplexJoin.setEnabled(relationshipMeta.isComplex());
+    wlComplexJoin.setEnabled(relationshipMeta.isComplex());
 
     /*
      if (relationshipMeta.isRegular())
