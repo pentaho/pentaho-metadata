bbc5f5eb1c945d96d25fb8d87971bb84066aaa23
aphillips <aphillips@pentaho.com>
2009-04-23 02:14:13 +0000

    stubbed PhysicalSchema

--- src/org/pentaho/metadata/model/PhysicalSchema.java
+++ src/org/pentaho/metadata/model/PhysicalSchema.java
@@ -1,5 +1,34 @@
 package org.pentaho.metadata.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.pentaho.pms.schema.PhysicalTable;
+
 public class PhysicalSchema extends Entity {
+
+  private PhysicalDataSource source;
+
+  private Collection<PhysicalTable> tables = new ArrayList<PhysicalTable>();
+
+  public PhysicalDataSource getSource() {
+    return source;
+  }
+
+  public void setSource(PhysicalDataSource source) {
+    this.source = source;
+  }
+
+  public Collection<PhysicalTable> getTables() {
+    return tables;
+  }
+
+  public void setTables(Collection<PhysicalTable> tables) {
+    this.tables = tables;
+  }
+  
+  public void addTable(PhysicalTable table) {
+    tables.add(table);
+  }
 
 }
