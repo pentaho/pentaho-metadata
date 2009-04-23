5af88c12ee2e460187161ac9e92f795e20e81ba7
aphillips <aphillips@pentaho.com>
2009-04-23 17:56:10 +0000

    made DataType an enum and removed length and precision fields for now.. if we need them they will come back during the evolution of the new model

--- src/org/pentaho/metadata/model/concept/types/DataType.java
+++ src/org/pentaho/metadata/model/concept/types/DataType.java
@@ -12,177 +12,52 @@
 */
 package org.pentaho.metadata.model.concept.types;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
 import org.pentaho.pms.messages.Messages;
 
-public class DataType
+public enum DataType
 {
-    public static final int DATA_TYPE_UNKNOWN   = 0;
-    public static final int DATA_TYPE_STRING    = 1;
-    public static final int DATA_TYPE_DATE      = 2;
-    public static final int DATA_TYPE_BOOLEAN   = 3;
-    public static final int DATA_TYPE_NUMERIC   = 4;
-    public static final int DATA_TYPE_BINARY    = 5;
-    public static final int DATA_TYPE_IMAGE     = 6;
-    public static final int DATA_TYPE_URL       = 7;
+    UNKNOWN(0, "Unknown", Messages.getString("DataType.USER_UNKNOWN_DESC")),
+    STRING(1, "String", Messages.getString("DataType.USER_STRING_DESC")),
+    DATE(2, "Date", Messages.getString("DataType.USER_DATE_DESC")),
+    BOOLEAN(3, "Boolean", Messages.getString("DataType.USER_BOOLEAN_DESC")),
+    NUMERIC(4, "Numeric", Messages.getString("DataType.USER_NUMERIC_DESC")),
+    BINARY(5, "Binary", Messages.getString("DataType.USER_BINARY_DESC")),
+    IMAGE(6, "Image", Messages.getString("DataType.USER_IMAGE_DESC")),
+    URL(7, "URL", Messages.getString("DataType.USER_URL_DESC"));
 
-    public static final DataType UNKNOWN = new DataType(DATA_TYPE_UNKNOWN);
-    public static final DataType STRING  = new DataType(DATA_TYPE_STRING);
-    public static final DataType DATE    = new DataType(DATA_TYPE_DATE);
-    public static final DataType BOOLEAN = new DataType(DATA_TYPE_BOOLEAN);
-    public static final DataType NUMERIC = new DataType(DATA_TYPE_NUMERIC);
-    public static final DataType BINARY  = new DataType(DATA_TYPE_BINARY);
-    public static final DataType IMAGE   = new DataType(DATA_TYPE_IMAGE);
-    public static final DataType URL     = new DataType(DATA_TYPE_URL);
-
-    private static final String typeCodes[] = { "Unknown", "String", "Date", "Boolean", "Numeric", "Binary", "Image", "URL", }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
-    private static final String typeDescriptions[] = {
-      Messages.getString("DataType.USER_UNKNOWN_DESC"),   //$NON-NLS-1$
-      Messages.getString("DataType.USER_STRING_DESC"),   //$NON-NLS-1$
-      Messages.getString("DataType.USER_DATE_DESC"),   //$NON-NLS-1$
-      Messages.getString("DataType.USER_BOOLEAN_DESC"),  //$NON-NLS-1$
-      Messages.getString("DataType.USER_NUMERIC_DESC"),   //$NON-NLS-1$
-      Messages.getString("DataType.USER_BINARY_DESC"),   //$NON-NLS-1$
-      Messages.getString("DataType.USER_IMAGE_DESC"),   //$NON-NLS-1$
-      Messages.getString("DataType.USER_URL_DESC"), }; //$NON-NLS-1$
-    public static final DataType[] types = new DataType[] { UNKNOWN, STRING, DATE, BOOLEAN, NUMERIC, BINARY, IMAGE, URL, };
-
-    private static final String SEPARATOR = ","; //$NON-NLS-1$
 
     private int type;
-    private int length;
-    private int precision;
+    private String name, description;
 
-    /**
-     * @param type
-     * @param length
-     * @param precision
-     */
-    public DataType(int type, int length, int precision)
-    {
-        super();
-        this.type = type;
-        this.length = length;
-        this.precision = precision;
-    }
-
-    /**
-     * @param type
-     * @param length
-     * @param precision
-     */
-    public DataType(int type)
-    {
-        super();
-        this.type = type;
-        this.length = -1;
-        this.precision = -1;
-    }
-
-    public String toString()
-    {
-        return getCode()+SEPARATOR+length+SEPARATOR+precision;
-    }
-
-    public static DataType fromString(String value)
-    {
-        String pieces[] = value.split(SEPARATOR);
-        if (pieces.length>0)
-        {
-            DataType settings = getType(pieces[0]);
-            if (pieces.length>1)
-            {
-                settings.setLength(Integer.parseInt(pieces[1]));
-                if (pieces.length>2)
-                {
-                    settings.setPrecision(Integer.parseInt(pieces[2]));
-                }
-            }
-
-            return settings;
-        }
-        return null;
-    }
-
-
-    public int getLength()
-    {
-        return length;
-    }
-
-    public void setLength(int length)
-    {
-        this.length = length;
-    }
-
-    public int getPrecision()
-    {
-        return precision;
-    }
-
-    public void setPrecision(int precision)
-    {
-        this.precision = precision;
-    }
-
-    public int getType()
-    {
-        return type;
-    }
-
-    public void setType(int type)
+    private DataType(int type, String name, String description)
     {
         this.type = type;
+        this.name = name;
+        this.description = description;
     }
 
-    public String getCode()
-    {
-        return typeCodes[type];
+    public int getType() {
+      return type;
     }
 
-    public String getDescription()
-    {
-        return typeDescriptions[type];
+    public void setType(int type) {
+      this.type = type;
     }
 
-    public static DataType getType(String description)
-    {
-        for (int i=0;i<typeDescriptions.length;i++)
-        {
-            if (typeDescriptions[i].equalsIgnoreCase(description))
-            {
-                return types[i];
-            }
-        }
-        for (int i=0;i<typeCodes.length;i++)
-        {
-            if (typeCodes[i].equalsIgnoreCase(description))
-            {
-                return types[i];
-            }
-        }
-        return UNKNOWN;
+    public String getName() {
+      return name;
     }
 
-    public boolean equals(Object obj) {
-      if (obj instanceof DataType == false) {
-        return false;
-      }
-      if (this == obj) {
-        return true;
-      }
-      DataType rhs = (DataType) obj;
-      return new EqualsBuilder().append(type, rhs.type).append(length, rhs.length).append(precision, rhs.precision)
-      .isEquals();
+    public void setName(String name) {
+      this.name = name;
     }
 
-    public int hashCode() {
-      return new HashCodeBuilder(23, 227).append(type).append(length).append(precision).toHashCode();
+    public String getDescription() {
+      return description;
     }
 
-    public static String[] getTypeDescriptions() {
-      return typeDescriptions.clone();
+    public void setDescription(String description) {
+      this.description = description;
     }
 
 }
