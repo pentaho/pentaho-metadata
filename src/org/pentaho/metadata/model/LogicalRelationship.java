6f84412c7667b4a5d7bd24b14d696dcd56fcb8f7
aphillips <aphillips@pentaho.com>
2009-04-23 02:08:10 +0000

    stubbed out LogicalRelationship

--- src/org/pentaho/metadata/model/LogicalRelationship.java
+++ src/org/pentaho/metadata/model/LogicalRelationship.java
@@ -1,5 +1,53 @@
 package org.pentaho.metadata.model;
 
 public class LogicalRelationship extends Entity {
+  
+  private LogicalTable fromTable, toTable;
+  private LogicalColumn fromColumn, toColumn;
+  /* where this relationship falls in relation to others */
+  private long ordinal;
+  
+  public LogicalTable getFromTable() {
+    return fromTable;
+  }
+
+  public void setFromTable(LogicalTable fromTable) {
+    this.fromTable = fromTable;
+  }
+
+  public LogicalTable getToTable() {
+    return toTable;
+  }
+
+  public void setToTable(LogicalTable toTable) {
+    this.toTable = toTable;
+  }
+
+  public LogicalColumn getFromColumn() {
+    return fromColumn;
+  }
+
+  public void setFromColumn(LogicalColumn fromColumn) {
+    this.fromColumn = fromColumn;
+  }
+
+  public LogicalColumn getToColumn() {
+    return toColumn;
+  }
+
+  public void setToColumn(LogicalColumn toColumn) {
+    this.toColumn = toColumn;
+  }
+
+  public long getOrdinal() {
+    return ordinal;
+  }
+
+  public void setOrdinal(long ordinal) {
+    this.ordinal = ordinal;
+  }
+
+  //TODO give this enum the correct set of supported join types
+  public enum JoinType { OUTER, INNER, LEFT, RIGHT }
 
 }
