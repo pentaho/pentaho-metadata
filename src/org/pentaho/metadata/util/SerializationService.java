ac38104801aeb0a1a7e9d3ab43a7f68795fb4c59
wgorman <wgorman@pentaho.com>
2009-04-28 17:52:22 +0000

    BISERVER-3063: Implemented initial metadata domain repository file store, along with a unit test that exercises the API.

--- src/org/pentaho/metadata/util/SerializationService.java
+++ src/org/pentaho/metadata/util/SerializationService.java
@@ -1,18 +1,33 @@
 package org.pentaho.metadata.util;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.pentaho.metadata.model.Domain;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class SerializationService {
+  
   public String serializeDomain(Domain domain) {
     XStream xstream = new XStream(new DomDriver());
     return xstream.toXML(domain);
+  }
+  
+  public void serializeDomain(Domain domain, OutputStream out) {
+    XStream xstream = new XStream(new DomDriver());
+    xstream.toXML(domain, out);
   }
   
   public Domain deserializeDomain(String xml) {
     XStream xstream = new XStream(new DomDriver());
     return (Domain)xstream.fromXML(xml);
   }
+  
+  public Domain deserializeDomain(InputStream xml) {
+    XStream xstream = new XStream(new DomDriver());
+    return (Domain)xstream.fromXML(xml);
+  }
+
 }
