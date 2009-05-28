0b438e63caf0ee0554866265b11121d5dbb5478c
rmansoor <rmansoor@pentaho.com>
2009-05-28 14:39:11 +0000

    Creating a new mode service to return a csv mode with a sample data. Also making changes to the ui to display the sample data

---
+++ src/org/pentaho/pms/service/CsvModelManagementService.java
@@ -0,0 +1,65 @@
+package org.pentaho.pms.service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.pentaho.metadata.model.Domain;
+import org.pentaho.metadata.util.InlineEtlModelGenerator;
+
+public class CsvModelManagementService {
+
+  public Domain generateModel(String modelName, String fileLocation, boolean headerPresent, String enclosure, String delimiter)
+      throws ModelManagementServiceException {
+    try {
+    InlineEtlModelGenerator generator = new InlineEtlModelGenerator(modelName, fileLocation, headerPresent, enclosure, delimiter);
+    return generator.generate();
+    } catch (Exception e) {
+      throw new ModelManagementServiceException(e);
+    }
+  }
+
+
+  public List<List<String>> getDataSample(String fileLocation, boolean headerPresent, String enclosure, String delimiter, int rowLimit) {
+    String line = null;
+    int row = 0;
+    List<List<String>> dataSample = new ArrayList<List<String>>(rowLimit);
+    File file = new File(fileLocation);
+    BufferedReader bufRdr = null;
+    try {
+      bufRdr = new BufferedReader(new FileReader(file));
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    //read each line of text file
+    try {
+      while((line = bufRdr.readLine()) != null && row < rowLimit)
+      {
+        StringTokenizer st = new StringTokenizer(line,delimiter);
+        List<String> rowData = new ArrayList<String>();
+        while (st.hasMoreTokens())
+        {
+          //get next token and store it in the list
+          rowData.add(st.nextToken());
+        }
+        if(headerPresent && row != 0 || !headerPresent) {
+          dataSample.add(rowData);  
+        }
+        row++;
+      }
+      //close the file
+      bufRdr.close();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    return dataSample;
+  }
+
+}
