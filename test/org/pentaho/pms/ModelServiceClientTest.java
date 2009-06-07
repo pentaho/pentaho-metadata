ef83319b0faf4a6ab433bb380d37dc748d306de9
wgorman <wgorman@pentaho.com>
2009-06-07 01:06:34 +0000

    converted test over to junit 4.0 so it could be ignored by CI

--- test/org/pentaho/pms/ModelServiceClientTest.java
+++ test/org/pentaho/pms/ModelServiceClientTest.java
@@ -7,7 +7,9 @@ import junit.framework.TestCase;
 
 import org.apache.axis2.client.Options;
 import org.apache.axis2.transport.http.HttpTransportProperties;
+import org.junit.Assert;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.pentaho.pms.schema.v3.client.ModelServiceClient;
 import org.pentaho.pms.schema.v3.model.Category;
 import org.pentaho.pms.schema.v3.model.Column;
@@ -22,16 +24,18 @@ import org.pentaho.pms.service.IModelService;
  *
  */
 @SuppressWarnings({"all"})
-public class ModelServiceClientTest extends TestCase {
+public class ModelServiceClientTest {//  extends TestCase {
 
+  @Ignore
+  @Test
   public void testListModelsClient() throws Exception {
     IModelService svc = getModelService("listModels");
     ModelEnvelope[] models = svc.listModels();
 
-    assertNotNull( "models is null", models ); //$NON-NLS-1$
+    Assert.assertNotNull( "models is null", models ); //$NON-NLS-1$
     
-    assertEquals( "Wrong number of models", 3, models.length ); //$NON-NLS-1$
-    assertEquals( "Wrong model name", "Human Resources", models[0].getName() ); //$NON-NLS-1$ //$NON-NLS-2$
+    Assert.assertEquals( "Wrong number of models", 3, models.length ); //$NON-NLS-1$
+    Assert.assertEquals( "Wrong model name", "Human Resources", models[0].getName() ); //$NON-NLS-1$ //$NON-NLS-2$
 
   }
   
@@ -53,74 +57,76 @@ public class ModelServiceClientTest extends TestCase {
   }
   
   @Ignore
+  @Test
   public void testGetModelClient() throws Exception {
 
     IModelService svc = getModelService("getModel");
     Model model = svc.getModel("steel-wheels", "BV_ORDERS", false);
 
-    assertNotNull( "models is null", model );
+    Assert.assertNotNull( "models is null", model );
     
-    assertEquals( "model id is wrong", "BV_ORDERS", model.getId() );
-    assertEquals( "model name is wrong", "Orders", model.getName() );
+    Assert.assertEquals( "model id is wrong", "BV_ORDERS", model.getId() );
+    Assert.assertEquals( "model name is wrong", "Orders", model.getName() );
     
     Category category = model.getRootCategory();
-    assertNotNull( "root category is null", category );
-    assertNull( "root category has columns", category.getColumns() );
-    assertEquals( "wrong # of subcategories", 4, category.getSubCategories().length );
+    Assert.assertNotNull( "root category is null", category );
+    Assert.assertNull( "root category has columns", category.getColumns() );
+    Assert.assertEquals( "wrong # of subcategories", 4, category.getSubCategories().length );
 
     category = category.getSubCategories()[2];
-    assertEquals( "category id is wrong", "CAT_PRODUCTS", category.getId() );
-    assertEquals( "category name is wrong", "Products", category.getName() );
-    assertEquals( "category description is wrong", "This category contains inforamtion about products.", category.getDescription() );
-    assertEquals( "category has wrong # of columns", 9, category.getColumns().length );
-    assertNull( "wrong # of subcategories", category.getSubCategories() );
+    Assert.assertEquals( "category id is wrong", "CAT_PRODUCTS", category.getId() );
+    Assert.assertEquals( "category name is wrong", "Products", category.getName() );
+    Assert.assertEquals( "category description is wrong", "This category contains inforamtion about products.", category.getDescription() );
+    Assert.assertEquals( "category has wrong # of columns", 9, category.getColumns().length );
+    Assert.assertNull( "wrong # of subcategories", category.getSubCategories() );
     
     Column column = (Column) category.getColumns()[1];
-    assertEquals( "column id is wrong", "BC_PRODUCTS_PRODUCTCODE", column.getId() );
-    assertEquals( "column name is wrong", "Product Code", column.getName() );
-    assertEquals( "column description is wrong", "BC_PRODUCTS_PRODUCTCODE", column.getDescription() );
-    assertNull( "column attributes wrong", column.getAttributes() );
-    assertEquals( "column data type wrong", "String", column.getDataType() );
-    assertEquals( "column field type wrong", "Dimension", column.getFieldType() );
+    Assert.assertEquals( "column id is wrong", "BC_PRODUCTS_PRODUCTCODE", column.getId() );
+    Assert.assertEquals( "column name is wrong", "Product Code", column.getName() );
+    Assert.assertEquals( "column description is wrong", "BC_PRODUCTS_PRODUCTCODE", column.getDescription() );
+    Assert.assertNull( "column attributes wrong", column.getAttributes() );
+    Assert.assertEquals( "column data type wrong", "String", column.getDataType() );
+    Assert.assertEquals( "column field type wrong", "Dimension", column.getFieldType() );
     
   }
   
   @Ignore
+  @Test
   public void testGetModelClientDeep() throws Exception {
 
     IModelService svc = getModelService("getModel");
     Model model = svc.getModel("steel-wheels", "BV_ORDERS", true);
 
-    assertNotNull( "models is null", model );
+    Assert.assertNotNull( "models is null", model );
     
-    assertEquals( "model id is wrong", "BV_ORDERS", model.getId() );
-    assertEquals( "model name is wrong", "Orders", model.getName() );
+    Assert.assertEquals( "model id is wrong", "BV_ORDERS", model.getId() );
+    Assert.assertEquals( "model name is wrong", "Orders", model.getName() );
     
     Category category = model.getRootCategory();
-    assertNotNull( "root category is null", category );
-    assertNull( "root category has columns", category.getColumns() );
-    assertEquals( "wrong # of subcategories", 4, category.getSubCategories().length );
+    Assert.assertNotNull( "root category is null", category );
+    Assert.assertNull( "root category has columns", category.getColumns() );
+    Assert.assertEquals( "wrong # of subcategories", 4, category.getSubCategories().length );
 
     category = category.getSubCategories()[2];
-    assertEquals( "category id is wrong", "CAT_PRODUCTS", category.getId() );
-    assertEquals( "category name is wrong", "Products", category.getName() );
-    assertEquals( "category description is wrong", "This category contains inforamtion about products.", category.getDescription() );
-    assertEquals( "category has wrong # of columns", 9, category.getColumns().length );
-    assertNull( "wrong # of subcategories", category.getSubCategories() );
+    Assert.assertEquals( "category id is wrong", "CAT_PRODUCTS", category.getId() );
+    Assert.assertEquals( "category name is wrong", "Products", category.getName() );
+    Assert.assertEquals( "category description is wrong", "This category contains inforamtion about products.", category.getDescription() );
+    Assert.assertEquals( "category has wrong # of columns", 9, category.getColumns().length );
+    Assert.assertNull( "wrong # of subcategories", category.getSubCategories() );
     
     Column column = (Column) category.getColumns()[1];
-    assertEquals( "column id is wrong", "BC_PRODUCTS_PRODUCTCODE", column.getId() );
-    assertEquals( "column name is wrong", "Product Code", column.getName() );
-    assertEquals( "column description is wrong", "BC_PRODUCTS_PRODUCTCODE", column.getDescription() );
-    assertNotNull( "column attributes wrong", column.getAttributes() );
-    assertEquals( "column attribute count wrong", 3, column.getAttributes().length );
-    assertEquals( "column data type wrong", "String", column.getDataType() );
-    assertEquals( "column field type wrong", "Dimension", column.getFieldType() );
+    Assert.assertEquals( "column id is wrong", "BC_PRODUCTS_PRODUCTCODE", column.getId() );
+    Assert.assertEquals( "column name is wrong", "Product Code", column.getName() );
+    Assert.assertEquals( "column description is wrong", "BC_PRODUCTS_PRODUCTCODE", column.getDescription() );
+    Assert.assertNotNull( "column attributes wrong", column.getAttributes() );
+    Assert.assertEquals( "column attribute count wrong", 3, column.getAttributes().length );
+    Assert.assertEquals( "column data type wrong", "String", column.getDataType() );
+    Assert.assertEquals( "column field type wrong", "Dimension", column.getFieldType() );
     
-    assertEquals( "attribute formula wrong", "formula", column.getAttributes()[1].getId() );
-    assertEquals( "attribute formula wrong", "PRODUCTCODE", column.getAttributes()[1].getValue() );
-    assertEquals( "attribute exact wrong", "exact", column.getAttributes()[0].getId() );
-    assertEquals( "attribute exact wrong", "false", column.getAttributes()[0].getValue() );
+    Assert.assertEquals( "attribute formula wrong", "formula", column.getAttributes()[1].getId() );
+    Assert.assertEquals( "attribute formula wrong", "PRODUCTCODE", column.getAttributes()[1].getValue() );
+    Assert.assertEquals( "attribute exact wrong", "exact", column.getAttributes()[0].getId() );
+    Assert.assertEquals( "attribute exact wrong", "false", column.getAttributes()[0].getValue() );
 
   }
   
