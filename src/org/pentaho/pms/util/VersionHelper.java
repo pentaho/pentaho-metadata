ca43acff49ab850e1adb4c5a5cfcff8646a3a5ed
wgorman <wgorman@pentaho.com>
2009-09-10 18:49:00 +0000

    [PMD-575] Updated splash screen

--- src/org/pentaho/pms/util/VersionHelper.java
+++ src/org/pentaho/pms/util/VersionHelper.java
@@ -27,27 +27,40 @@ public class VersionHelper implements IVersionHelper {
   }
 
   public String getVersionInformation(Class clazz) {
+    return getVersionInformation(clazz, true);
+  }
+  
+  public String getVersionInformation(Class clazz, boolean includeTitle) {
     // The following two lines read from the MANIFEST.MF
     String implTitle = clazz.getPackage().getImplementationTitle();
     String implVersion = clazz.getPackage().getImplementationVersion();
     if (implVersion != null) {
       // If we're in a .jar file, then we can return the version information
       // from the .jar file.
-      return implTitle + " " + implVersion; //$NON-NLS-1$
+      if (includeTitle) {
+        return implTitle + " " + implVersion; //$NON-NLS-1$
+      } else {
+        return implVersion;
+      }
     } else {
       // We're not in a .jar file - try to find the build-res/version file and
       // read the version information from that.
       try {
         ResourceBundle bundle = ResourceBundle.getBundle("build-res.version"); //$NON-NLS-1$
         StringBuffer buff = new StringBuffer();
-        buff
-            .append(bundle.getString("impl.title")).append(' ').append(bundle.getString("release.major.number")).append('.').append(bundle.getString("release.minor.number")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-        buff
-            .append('.')
-            .append(bundle.getString("release.milestone.number")).append('.').append(bundle.getString("release.build.number")).append(" (class)"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+        if (includeTitle) {
+          buff.append(bundle.getString("impl.title")).append(' ');  //$NON-NLS-1$
+        }
+        
+        buff.append(bundle.getString("release.major.number")).append('.').append(bundle.getString("release.minor.number")); //$NON-NLS-1$ //$NON-NLS-2$
+        buff.append('.').append(bundle.getString("release.milestone.number")).append('.').append(bundle.getString("release.build.number")).append(" (class)"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         return buff.toString();
       } catch (Exception ex) {
-        return "Pentaho Metadata Editor - No Version Information Available"; //$NON-NLS-1$
+        if (includeTitle) {
+          return "Pentaho Metadata Editor - No Version Information Available"; //$NON-NLS-1$          
+        } else {
+          return "No Version Information Available"; //$NON-NLS-1$
+        }
       }
     }
   }
