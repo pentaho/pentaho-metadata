ab1bf9f8f0b50f5166ad24b3e16c2e36d8ba9612
nbaker <nbaker@pentaho.com>
2010-04-16 04:11:13 +0000

    [PMD-620] Defaulting file encoding to UTF-8. Added fallback to latin-1 to handle legacy domain files.

--- src/org/pentaho/metadata/util/SerializationService.java
+++ src/org/pentaho/metadata/util/SerializationService.java
@@ -16,34 +16,51 @@
  */
 package org.pentaho.metadata.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import org.pentaho.metadata.model.Domain;
 
 import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.StreamException;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class SerializationService {
   
   public String serializeDomain(Domain domain) {
-    XStream xstream = new XStream(new DomDriver());
+    
+    XStream xstream = new XStream(new DomDriver("UTF-8"));  //$NON-NLS-1$
     return xstream.toXML(domain);
   }
   
   public void serializeDomain(Domain domain, OutputStream out) {
-    XStream xstream = new XStream(new DomDriver());
+    XStream xstream = new XStream(new DomDriver("UTF-8"));  //$NON-NLS-1$
     xstream.toXML(domain, out);
   }
   
   public Domain deserializeDomain(String xml) {
-    XStream xstream = new XStream(new DomDriver());
-    return (Domain)xstream.fromXML(xml);
+    
+    try{
+      XStream xstream = new XStream(new DomDriver("UTF-8"));  //$NON-NLS-1$
+      return (Domain)xstream.fromXML(xml);
+    } catch(StreamException e){
+      XStream xstream = new XStream(new DomDriver("ISO-8859-1")); //$NON-NLS-1$
+      return (Domain)xstream.fromXML(xml);
+    }
   }
   
   public Domain deserializeDomain(InputStream xml) {
-    XStream xstream = new XStream(new DomDriver());
-    return (Domain)xstream.fromXML(xml);
+    
+    try{
+      XStream xstream = new XStream(new DomDriver("UTF-8"));  //$NON-NLS-1$
+      return (Domain)xstream.fromXML(xml);
+    } catch(StreamException e){
+      XStream xstream = new XStream(new DomDriver("ISO-8859-1")); //$NON-NLS-1$
+      return (Domain)xstream.fromXML(xml);
+    }
   }
 
 }
