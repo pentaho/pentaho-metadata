e67a41e28c275dff1a725c405db2e9e581775603
nbaker <nbaker@pentaho.com>
2010-04-20 19:06:01 +0000

    [AGILEBI-315] Fixed hierarchy name property.

--- src/org/pentaho/metadata/util/MondrianModelExporter.java
+++ src/org/pentaho/metadata/util/MondrianModelExporter.java
@@ -88,9 +88,11 @@ public class MondrianModelExporter
                 OlapHierarchy olapHierarchy = (OlapHierarchy) olapHierarchies.get(h);
                 xml.append("    <Hierarchy"); //$NON-NLS-1$
 
-                xml.append(" allMemberName=\""); //$NON-NLS-1$
-                xml.append(olapHierarchy.getName());
-                xml.append("\""); //$NON-NLS-1$
+                if(StringUtils.isNotEmpty(olapHierarchy.getName())){
+                  xml.append(" name=\""); //$NON-NLS-1$
+                  xml.append(olapHierarchy.getName());
+                  xml.append("\""); //$NON-NLS-1$
+                }
                 
                 xml.append(" hasAll=\""); //$NON-NLS-1$
                 xml.append(olapHierarchy.isHavingAll()?"true":"false"); //$NON-NLS-1$ //$NON-NLS-2$
