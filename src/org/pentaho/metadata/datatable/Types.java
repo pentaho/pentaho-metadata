b6d4e9e3f97c94de78d3ec6904ae52dc00f09064
James Dixon <jdixon@pentaho.com>
2012-06-06 19:43:21 +0000

    Added a types class for the data table

---
+++ src/org/pentaho/metadata/datatable/Types.java
@@ -0,0 +1,10 @@
+package org.pentaho.metadata.datatable;
+
+public class Types {
+
+	public static final String TYPE_STRING = "string";
+	public static final String TYPE_NUMERIC = "number";
+	public static final String TYPE_DATE = "date";
+	public static final String TYPE_BOOLEAN = "boolean";
+	
+}
