503711d26f8ec045da6deb6c783d2e37e5ba8815
mkambol <mkambol@gmail.com>
2013-10-01 09:17:53 -0400

    [BISERVER-10259]  Exported Mondrian schema should not include quote characters surrounding table or schema name. (cherry picked from commit a500781)

--- test/org/pentaho/metadata/MondrianModelExporterTest.java
+++ test/org/pentaho/metadata/MondrianModelExporterTest.java
@@ -16,62 +16,7 @@ public class MondrianModelExporterTest {
 
   @Test
   public void testDegenerateViewGen() throws Exception {
-    
-    List<OlapDimension> dimensions = new ArrayList<OlapDimension>();
-    OlapDimension dimension = new OlapDimension();
-    dimension.setName("Dim1");
-    
-    List<OlapHierarchy> hierarchies = new ArrayList<OlapHierarchy>();
-    OlapHierarchy hierarchy = new OlapHierarchy();
-    hierarchy.setName("Hier1");
-    List<OlapHierarchyLevel> hierarchyLevels = new ArrayList<OlapHierarchyLevel>();
-    OlapHierarchyLevel level = new OlapHierarchyLevel();
-    level.setName("Lvl1");
-    hierarchyLevels.add(level);
-    
-    hierarchy.setHierarchyLevels(hierarchyLevels);
-    hierarchies.add(hierarchy);
-    dimension.setHierarchies(hierarchies);
-    
-    dimensions.add(dimension);
-    
-    List<OlapCube> cubes = new ArrayList<OlapCube>();
-    OlapCube cube = new OlapCube();
-    cube.setName("Cube1");
-    cubes.add(cube);
-    
-    List<OlapMeasure> measures = new ArrayList<OlapMeasure>();
-    OlapMeasure measure = new OlapMeasure();
-    measure.setName("Meas1");
-    measures.add(measure);
-    cube.setOlapMeasures(measures);
-    
-    List<OlapDimensionUsage> dimensionUsages = new ArrayList<OlapDimensionUsage>();
-
-    OlapDimensionUsage dimUsage = new OlapDimensionUsage();
-    dimensionUsages.add(dimUsage);
-    cube.setOlapDimensionUsages(dimensionUsages);
-    
-    dimUsage.setName("Dim1");
-    dimUsage.setOlapDimension(dimension);
-    
-    LogicalModel businessModel = TestHelper.buildDefaultModel();
-    LogicalTable logicalTable = businessModel.getLogicalTables().get(0); 
-    hierarchy.setLogicalTable(logicalTable);
-    List<LogicalColumn> logicalColumns = new ArrayList<LogicalColumn>();
-    level.setReferenceColumn(logicalTable.getLogicalColumns().get(0));
-    level.setLogicalColumns(new ArrayList<LogicalColumn>());
-    logicalTable.setProperty(SqlPhysicalTable.TARGET_TABLE, "select * from customer");
-    LogicalColumn m = logicalTable.getLogicalColumns().get(0);
-    m.setAggregationType(AggregationType.SUM);
-    measure.setLogicalColumn(m);
-    businessModel.getLogicalTables().get(0).setProperty(SqlPhysicalTable.TARGET_TABLE_TYPE, TargetTableType.INLINE_SQL);
-    cube.setLogicalTable(businessModel.getLogicalTables().get(0));
-    
-    businessModel.setProperty("olap_dimensions", dimensions);
-    businessModel.setProperty("olap_cubes", cubes);
-    
-    businessModel.setName(new LocalizedString("en_US", "model"));
+    LogicalModel businessModel = getTestModel(TargetTableType.INLINE_SQL, "select * from customer", "");
     MondrianModelExporter exporter = new MondrianModelExporter(businessModel, "en_US");
     String data = exporter.createMondrianModelXML();
     
@@ -103,6 +48,58 @@ public class MondrianModelExporterTest {
   }
 
   @Test
+  public void testUnquotedTargetTable() throws Exception {
+      LogicalModel businessModel = getTestModel(TargetTableType.TABLE, "tableName", "schemaName");
+      MondrianModelExporter exporter = new MondrianModelExporter(businessModel, "en_US");
+      String data = exporter.createMondrianModelXML();
+
+      TestHelper.assertEqualsIgnoreWhitespaces(
+          "<Schema name=\"model\">\n" +
+              "  <Dimension name=\"Dim1\">\n" +
+              "    <Hierarchy name=\"Hier1\" hasAll=\"false\">\n" +
+              "    <Table name=\"tableName\" schema=\"schemaName\" />\n" +
+              "      <Level name=\"Lvl1\" uniqueMembers=\"false\" column=\"pc1\" type=\"Numeric\">\n" +
+              "      </Level>\n" +
+              "    </Hierarchy>\n" +
+              "  </Dimension>\n" +
+              "  <Cube name=\"Cube1\">\n" +
+              "    <Table name=\"tableName\" schema=\"schemaName\" />\n" +
+              "    <DimensionUsage name=\"Dim1\" source=\"Dim1\" foreignKey=\"pc2\"/>\n" +
+              "    <Measure name=\"bc1\" column=\"pc1\" aggregator=\"sum\" formatString=\"Standard\"/>\n" +
+              "  </Cube>\n" +
+              "</Schema>",
+          data
+      );
+  }
+
+  @Test
+  public void testQuotedTargetTable() throws Exception {
+      LogicalModel businessModel = getTestModel(TargetTableType.TABLE, "`tableName`", "`schemaName`");
+      MondrianModelExporter exporter = new MondrianModelExporter(businessModel, "en_US");
+      String data = exporter.createMondrianModelXML();
+
+      TestHelper.assertEqualsIgnoreWhitespaces(
+          "<Schema name=\"model\">\n" +
+              "  <Dimension name=\"Dim1\">\n" +
+              "    <Hierarchy name=\"Hier1\" hasAll=\"false\">\n" +
+              "    <Table name=\"tableName\" schema=\"schemaName\"/>\n" +
+              "      <Level name=\"Lvl1\" uniqueMembers=\"false\" column=\"pc1\" type=\"Numeric\">\n" +
+              "      </Level>\n" +
+              "    </Hierarchy>\n" +
+              "  </Dimension>\n" +
+              "  <Cube name=\"Cube1\">\n" +
+              "    <Table name=\"tableName\" schema=\"schemaName\" />\n" +
+              "    <DimensionUsage name=\"Dim1\" source=\"Dim1\" foreignKey=\"pc2\"/>\n" +
+              "    <Measure name=\"bc1\" column=\"pc1\" aggregator=\"sum\" formatString=\"Standard\"/>\n" +
+              "  </Cube>\n" +
+              "</Schema>",
+          data
+      );
+  }
+
+
+
+    @Test
   public void testLevelAnnotations() throws Exception {
 
     List<OlapDimension> dimensions = new ArrayList<OlapDimension>();
@@ -307,6 +304,72 @@ public class MondrianModelExporterTest {
         "</Schema>",
         data
     );
+  }
+
+  private LogicalModel getTestModel(
+      TargetTableType tableType,
+      String targetTable,
+      String targetSchema)
+
+  {
+      List<OlapDimension> dimensions = new ArrayList<OlapDimension>();
+      OlapDimension dimension = new OlapDimension();
+      dimension.setName("Dim1");
+
+      List<OlapHierarchy> hierarchies = new ArrayList<OlapHierarchy>();
+      OlapHierarchy hierarchy = new OlapHierarchy();
+      hierarchy.setName("Hier1");
+      List<OlapHierarchyLevel> hierarchyLevels = new ArrayList<OlapHierarchyLevel>();
+      OlapHierarchyLevel level = new OlapHierarchyLevel();
+      level.setName("Lvl1");
+      hierarchyLevels.add(level);
+
+      hierarchy.setHierarchyLevels(hierarchyLevels);
+      hierarchies.add(hierarchy);
+      dimension.setHierarchies(hierarchies);
+
+      dimensions.add(dimension);
+
+      List<OlapCube> cubes = new ArrayList<OlapCube>();
+      OlapCube cube = new OlapCube();
+      cube.setName("Cube1");
+      cubes.add(cube);
+
+      List<OlapMeasure> measures = new ArrayList<OlapMeasure>();
+      OlapMeasure measure = new OlapMeasure();
+      measure.setName("Meas1");
+      measures.add(measure);
+      cube.setOlapMeasures(measures);
+
+      List<OlapDimensionUsage> dimensionUsages = new ArrayList<OlapDimensionUsage>();
+
+      OlapDimensionUsage dimUsage = new OlapDimensionUsage();
+      dimensionUsages.add(dimUsage);
+      cube.setOlapDimensionUsages(dimensionUsages);
+
+      dimUsage.setName("Dim1");
+      dimUsage.setOlapDimension(dimension);
+
+      LogicalModel businessModel = TestHelper.buildDefaultModel();
+      LogicalTable logicalTable = businessModel.getLogicalTables().get(0);
+      hierarchy.setLogicalTable(logicalTable);
+      List<LogicalColumn> logicalColumns = new ArrayList<LogicalColumn>();
+      level.setReferenceColumn(logicalTable.getLogicalColumns().get(0));
+      level.setLogicalColumns(new ArrayList<LogicalColumn>());
+      logicalTable.setProperty(SqlPhysicalTable.TARGET_TABLE, targetTable);
+      logicalTable.setProperty(SqlPhysicalTable.TARGET_SCHEMA, targetSchema);
+
+      LogicalColumn m = logicalTable.getLogicalColumns().get(0);
+      m.setAggregationType(AggregationType.SUM);
+      measure.setLogicalColumn(m);
+      businessModel.getLogicalTables().get(0).setProperty(SqlPhysicalTable.TARGET_TABLE_TYPE, tableType);
+      cube.setLogicalTable(businessModel.getLogicalTables().get(0));
+
+      businessModel.setProperty("olap_dimensions", dimensions);
+      businessModel.setProperty("olap_cubes", cubes);
+
+      businessModel.setName(new LocalizedString("en_US", "model"));
+      return businessModel;
   }
 
 }
