5ceb1a8f7a0310e8ee96e217eb59c0ff996024a9
Diogo Mariano <dmariano@pentaho.com>
2015-10-02 16:40:10 +0100

    [BACKLOG-5407] - Classify Unit Test as Integration Tests. Tests with file system access marked as Integration Test - Standardized test dir, removing test directory override, allowing Integration Tests to run properly - Re-enabled and fixed ChartItemLabelGeneratorIT test

---
+++ test-src/org/pentaho/pms/mql/dialect/MSAccessDialectIT.java
@@ -0,0 +1,15 @@
+package org.pentaho.pms.mql.dialect;
+
+import org.pentaho.pms.MetadataTestBase;
+
+public class MSAccessDialectIT extends MetadataTestBase {
+  public void testLimitSQL() {
+    assertSelect( "SELECT DISTINCT TOP 10 t.id FROM TABLE t WHERE ( ( t.id is null ) ) ORDER BY t.id ASC",
+        new MSAccessDialect(), createLimitedQuery() );
+  }
+
+  public void testNoLimitSQL() {
+    assertSelect( "SELECT DISTINCT t.id FROM TABLE t WHERE ( ( t.id is null ) ) ORDER BY t.id ASC",
+        new MSAccessDialect(), createUnlimitedQuery() );
+  }
+}
