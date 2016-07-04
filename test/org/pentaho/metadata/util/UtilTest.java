93a3081216dcdc22dcab814b54b28f70362f5fa4
Aleksandr Kozlov <Aleksandr_Kozlov1@epam.com>
2016-07-04 10:35:04 +0400

    [SP-2767] - Backport of BISERVER-13159 - Upgrade issue from 5.1 to 5.4 when SQL datasource created via Data Access Wizard has Japanese Columns in it. (5.4 Suite)
    - List of unacceptable characters was added for validateId() method
    - UTs
    - MQL_RESERVED_CHARS constant was added
    - Updated the Headers

--- test/org/pentaho/metadata/util/UtilTest.java
+++ test/org/pentaho/metadata/util/UtilTest.java
@@ -1,13 +1,29 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2016 Pentaho Corporation..  All rights reserved.
+ */
 package org.pentaho.metadata.util;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class UtilTest {
 
@@ -39,6 +55,9 @@ public class UtilTest {
     assertTrue( Util.validateId( "qwerty_1" ) );
     assertTrue( Util.validateId( "qwerty_$1" ) );
     assertTrue( Util.validateId( "qWerTy_$1" ) );
+    assertTrue( Util.validateId( "Кириллические_символы" ) );
+    assertTrue( Util.validateId( "日本の手紙" ) );
+    assertTrue( Util.validateId( "caractères_français" ) );
   }
 
   @Test
