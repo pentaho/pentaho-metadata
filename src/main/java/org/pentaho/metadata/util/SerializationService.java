f9756fcabbfc60a573780a3d12a6da855321159e
André Ramos <andre.ramos@hitachivantara.com>
2023-02-02 18:27:32 +0000

    [SP-6250] - Backport of PPP-4704 - Vulnerable Component: xstream-1.4.17 (9.3 Suite)

--- src/main/java/org/pentaho/metadata/util/SerializationService.java
+++ src/main/java/org/pentaho/metadata/util/SerializationService.java
@@ -19,6 +19,7 @@ package org.pentaho.metadata.util;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import com.thoughtworks.xstream.io.xml.AbstractXmlDriver;
 import org.pentaho.metadata.model.Domain;
 
 import com.thoughtworks.xstream.XStream;
@@ -29,24 +30,25 @@ public class SerializationService {
 
   public String serializeDomain( Domain domain ) {
 
-    XStream xstream = new XStream( new DomDriver() ); //$NON-NLS-1$
+
+    XStream xstream = createXStreamWithAllowedTypes( new DomDriver(), null);
     return xstream.toXML( domain );
   }
 
   public void serializeDomain( Domain domain, OutputStream out ) {
-    XStream xstream = new XStream( new DomDriver() ); //$NON-NLS-1$
+    XStream xstream = createXStreamWithAllowedTypes( new DomDriver() );
     xstream.toXML( domain, out );
   }
 
   public Domain deserializeDomain( String xml ) {
 
     try {
-      XStream xstream = new XStream( new DomDriver() ); //$NON-NLS-1$
+      XStream xstream = createXStreamWithAllowedTypes(new DomDriver(), Domain.class );
       return (Domain) xstream.fromXML( xml );
     } catch ( StreamException e ) {
       // try to load ASCII. This addresses sample domains being mixed with customer created ones in
       // a different encoding.
-      XStream xstream = new XStream( new DomDriver( "ISO-8859-1" ) ); //$NON-NLS-1$
+      XStream xstream = createXStreamWithAllowedTypes( new DomDriver("ISO-8859-1" ), Domain.class );
       return (Domain) xstream.fromXML( xml );
     }
   }
@@ -54,14 +56,23 @@ public class SerializationService {
   public Domain deserializeDomain( InputStream stream ) {
 
     try {
-      XStream xstream = new XStream( new DomDriver() ); //$NON-NLS-1$
+      XStream xstream = createXStreamWithAllowedTypes( new DomDriver(), Domain.class );
       return (Domain) xstream.fromXML( stream );
     } catch ( StreamException e ) {
       // try to load ASCII. This addresses sample domains being mixed with customer created ones in
       // a different encoding.
-      XStream xstream = new XStream( new DomDriver( "ISO-8859-1" ) );
+      XStream xstream = createXStreamWithAllowedTypes( new DomDriver("ISO-8859-1" ), Domain.class );
       return (Domain) xstream.fromXML( stream );
     }
   }
+
+  public static XStream createXStreamWithAllowedTypes( AbstractXmlDriver driver, Class ... classes ) {
+    XStream xstream = driver == null ? new XStream() : new XStream( driver );
+      if( classes != null ) {
+        xstream.allowTypes( classes );
+      }
+      return xstream;
+  }
+
 
 }
