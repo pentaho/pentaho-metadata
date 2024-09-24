83af32f0aec14a6e99a5f97acea442b247361f28
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 16:29:31 -0400

    HNC-766: updated license and copyright headers

--- src/main/java/org/pentaho/metadata/util/XmiParser.java
+++ src/main/java/org/pentaho/metadata/util/XmiParser.java
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 // CHECKSTYLE:FileLength:OFF
 /*
  * This program is free software; you can redistribute it and/or modify it under the
